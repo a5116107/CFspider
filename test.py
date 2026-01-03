d32b46ca285f971be1cf2dd86cc36cdbdcf1a2ef
test01 <test01@kami666.xyz>
2026-01-04 01:43:36 +0800

    chore: ignore test files

--- test.py
+++
@@ -1,9 +0,0 @@
-import cfspider
-import requests
-
-worker_url = "ip.kami666.xyz"
-cf_response = cfspider.get("https://httpbin.org/ip", cf_proxies=worker_url)
-req_response = requests.get("https://httpbin.org/ip")
-
-print(cf_response.text)
-print(req_response.text)
